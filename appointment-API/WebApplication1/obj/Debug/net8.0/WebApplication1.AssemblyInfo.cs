d8d1e71ccbc915cec066f92eecdead4a5cdf45be
karbejha <mohamad.karbejha@gmail.com>
2024-11-13 17:12:12 +0300

    add gitignore

--- appointment-API/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ appointment-API/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7164441c4cce2cab399203af9409fb5b68491e1b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8369ff8d0a4d1ec6f578fd0bd3f4689bdc14ad4")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
