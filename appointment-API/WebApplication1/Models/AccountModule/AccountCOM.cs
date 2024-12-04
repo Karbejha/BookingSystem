479b332a9914e491d3b3d07747766fb99c8731a0
karbejha <mohamad.karbejha@gmail.com>
2024-12-04 16:39:07 +0300

    Enhance hashing passwords in Signup and Signin

--- appointment-API/WebApplication1/Models/AccountModule/AccountCOM.cs
+++ appointment-API/WebApplication1/Models/AccountModule/AccountCOM.cs
@@ -2,43 +2,15 @@
 using WebApplication1.Models.Database;
 using MySql.Data.MySqlClient;
 
+// using this for hashing password
+using Microsoft.AspNetCore.Identity;
+
+
 namespace WebApplication1.Models.AccountModule
 {
     public class AccountCOM
     {
-        public async Task<LoginResponse> Login(LoginRequest oModel)
-        {
-            LoginResponse response = new LoginResponse();
-            using (AppointmentdbContext db = new AppointmentdbContext())
-            {
-                User oUser = await db.Users.Where(x => x.Username == oModel.UserName && x.Password == oModel.Password).FirstOrDefaultAsync();
-                if (oUser != null && oUser.Id > 0)
-                {
-                    if (oUser.IsActive == 1)
-                    {
-                        response.UserID = oUser.Id;
-                        response.UserName = oUser.Username;
-                        response.Email = oUser.Email;
-                        response.Type = oUser.Type;
-                        response.IsActive = true;
-                        response.StatusCode = 200;
-                        response.Message = "Success";
-                    }
-                    else
-                    {
-                        response.StatusCode = 403;
-                        response.Message = "Your account is blocked";
-                    }
-
-                }
-                else
-                {
-                    response.StatusCode = 401;
-                    response.Message = "Your account is not exist";
-                }
-            }
-            return response;
-        }
+        private readonly PasswordHasher<User> _passwordHasher = new PasswordHasher<User>();
         public async Task<SignupResponse> Signup(SignupRequest oModel)
         {
             SignupResponse response = new SignupResponse();
@@ -59,12 +31,15 @@ namespace WebApplication1.Models.AccountModule
                         {
                             Email = oModel.Email,
                             FullName = oModel.UserName,
-                            Password = oModel.Password,
                             Username = oModel.UserName,
                             IsActive = 1,
-                            Type =2,
+                            Type = 2,
                             CreatedAt = DateTime.Now,
                         };
+
+                        // Hash the password
+                        newUser.Password = _passwordHasher.HashPassword(newUser, oModel.Password);
+
                         db.Users.Add(newUser);
                         await db.SaveChangesAsync();
 
@@ -74,15 +49,62 @@ namespace WebApplication1.Models.AccountModule
                         response.Type = newUser.Type;
                         response.IsActive = true;
                         response.StatusCode = 201;
-                        response.Message = "user created Successful";
+                        response.Message = "User created successfully";
                     }
                 }
             }
-            catch (Exception e) { 
+            catch (Exception e)
+            {
+                response.StatusCode = 500;
+                response.Message = "An error occurred while creating the user.";
             }
             return response;
-
         }
+
+        public async Task<LoginResponse> Login(LoginRequest oModel)
+        {
+            LoginResponse response = new LoginResponse();
+            using (AppointmentdbContext db = new AppointmentdbContext())
+            {
+                User oUser = await db.Users.FirstOrDefaultAsync(x => x.Username == oModel.UserName);
+                if (oUser != null)
+                {
+                    // Verify the password
+                    var verificationResult = _passwordHasher.VerifyHashedPassword(oUser, oUser.Password, oModel.Password);
+                    if (verificationResult == PasswordVerificationResult.Success)
+                    {
+                        if (oUser.IsActive == 1)
+                        {
+                            response.UserID = oUser.Id;
+                            response.UserName = oUser.Username;
+                            response.Email = oUser.Email;
+                            response.Type = oUser.Type;
+                            response.IsActive = true;
+                            response.StatusCode = 200;
+                            response.Message = "Success";
+                        }
+                        else
+                        {
+                            response.StatusCode = 403;
+                            response.Message = "Your account is blocked";
+                        }
+                    }
+                    else
+                    {
+                        response.StatusCode = 401;
+                        response.Message = "Invalid username or password";
+                    }
+                }
+                else
+                {
+                    response.StatusCode = 401;
+                    response.Message = "Your account does not exist";
+                }
+            }
+            return response;
+        }
+
+
         public async Task<UserDetailsResponse> GetUserDetails(int UserID)
         {
             UserDetailsResponse response = new UserDetailsResponse();
