56024c21fd021992574c82076f359d817304719f
karbejha <mohamad.karbejha@gmail.com>
2024-12-16 17:48:16 +0300

    Add complete and failed message after signup process

--- appointment-booking-system/src/app/signup/signup.component.ts
+++ appointment-booking-system/src/app/signup/signup.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
+import { RedirectCommand, Router, RouterModule } from '@angular/router';
 import { AuthService } from '../auth.service';
 import { HttpClient } from '@angular/common/http';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-signup',
@@ -21,7 +22,7 @@ export class SignupComponent {
   showSuccessMessage: boolean = false;
   isSubmitting: boolean = false;
 
-  constructor(private authService: AuthService, private http:HttpClient) { }
+  constructor(private authService: AuthService, private http:HttpClient,private router: Router) { }
   onInputChange(event: Event, field: string): void {
     const inputElement = event.target as HTMLInputElement;
     switch (field) {
@@ -47,28 +48,41 @@ export class SignupComponent {
   }
 
   
-signup(): void {
-  this.isSubmitting = true;
-  this.authService.signup(this.username, this.FullName, this.email, this.password).then(
-    (response) => {
-      console.log('Signup successful:');
-      this.isSubmitting = false;
-      this.showSuccessMessage = true; // Show success message
-      
-      // Hide the message after a few seconds (optional)
-      setTimeout(() => {
-        this.showSuccessMessage = false;
-      }, 9000);
-    },
-  ).catch(
-    (error) => {
-      console.error('Signup failed:', error);
-      this.errorMessage = 'Signup failed. Please try again.';
-      this.isSubmitting = false;
-    }
-  );
-}
-
+  signup(): void {
+    this.isSubmitting = true;
+    this.authService.signup(this.username, this.FullName, this.email, this.password).then(
+      (response) => {
+        console.log('Signup successful:');
+        this.isSubmitting = false;
+  
+        // Show SweetAlert2 success message
+        Swal.fire({
+          title: 'Sign up successful!',
+          text: 'You have been successfully registered, Please login',
+          icon: 'success',
+          confirmButtonText: 'OK'
+        }).then(() => {
+          // Redirect to login page after user clicks 'OK'
+          this.router.navigate(['/login']);
+        });
+      }
+    ).catch(
+      (error) => {
+        console.error('Signup failed:', error);
+        this.errorMessage = 'Signup failed. Please try again.';
+        this.isSubmitting = false;
+  
+        // Show SweetAlert2 error message if the signup fails
+        Swal.fire({
+          title: 'Error!',
+          text: 'There was an error signing up. Please try again.',
+          icon: 'error',
+          confirmButtonText: 'OK'
+        });
+      }
+    );
+  }
+  
   
 }
 
