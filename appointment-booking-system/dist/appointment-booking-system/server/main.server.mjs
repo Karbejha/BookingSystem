db376ff22ce64b4c982866a7ff9dff7f171f49f7
karbejha <mohamad.karbejha@gmail.com>
2025-01-07 11:59:44 +0300

    Add prerendered routes and server files for appointment booking system

---
+++ appointment-booking-system/dist/appointment-booking-system/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-QRHNFC7Y.mjs";import"./chunk-KDZSEJCX.mjs";import"./chunk-W7A2EQTM.mjs";import"./chunk-UMTYLU74.mjs";import"./chunk-4MHTPWTV.mjs";import"./chunk-2PTEULGL.mjs";import"./chunk-NDYDZJSS.mjs";export{a as default};
